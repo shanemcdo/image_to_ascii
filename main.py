d1cf1618c462e06d07f976b5401f6e78d0f35a3b
Shane McDonough <shanemcdman@gmail.com>
2021-02-11 13:12:14 -0800

    Allow the output and size flags

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def convert_to_ascii(filename: str, scale: (int, int) = (1, 2)) -> str:
     img = Image.open(filename)
     img = ImageOps.grayscale(img)
     width, height = img.size
-    img = img.resize((width // scale[0], height // scale[1]))
+    img = img.resize((int(width // scale[0]), int(height // scale[1])))
     arr = np.array(img)
     brightest_pixel = -1
     darkest_pixel = 99999
@@ -46,7 +46,22 @@ def main():
         sys.argv.pop(0)
         if not len(sys.argv):
             raise ValueError('Not enough arguments')
+        i = 0
+        while i < len(sys.argv):
+            if sys.argv[i].lower() in ['-o', '--output']:
+                sys.argv.pop(i)
+                output_file = sys.argv.pop(i)
+            elif sys.argv[i].lower() in ['-s', '--size']:
+                sys.argv.pop(i)
+                scale = float(sys.argv.pop(i)), float(sys.argv.pop(i))
+            else:
+                i += 1
         ascii_image = convert_to_ascii(sys.argv[0], scale)
+        if output_file:
+            with open(output_file, 'w') as f:
+                f.write(ascii_image)
+        else:
+            print(ascii_image)
     except Exception as e:
         print(f'Error: {e}\n' + main.__doc__)
 
