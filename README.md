9ed394579b3537c113c5a314ac499dd60d16d7cf
Shane McDonough <shanemcdman@gmail.com>
2021-02-11 13:57:11 -0800

    Add -r and --reverse to docs

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ An application that converts images into ASCII representations of them
 ### flags:
 - -o {output filename} or --output {output filname}: outputs a file with the passed name instead of printing
 - -s {width divisor} {height divisor} or --size {width divisor} {height divisor}: scales the height and width of the output
+- -r or --reverse: reverses the ascii scale so the characters are the most dense where the image is the darkest (used for dark text on light background)
--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def main():
         flags:
             -o {output filename} or --output {output filname}: outputs a file with the passed name instead of printing
             -s {width divisor} {height divisor} or --size {width divisor} {height divisor}: scales the height and width of the output
+            -r or --reverse: reverses the ascii scale so the characters are the most dense where the image is the darkest (used for dark text on light background)
     """
     try:
         output_file = None
