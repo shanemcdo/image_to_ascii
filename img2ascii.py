0b48b734d6e856adb6f17c3179368284e289b183
Shane McDonough <shanemcdman@gmail.com>
2022-10-13 21:42:23 -0700

    Add ability to print gifs in img2ascii

--- img2ascii.py
+++ img2ascii.py
@@ -3,10 +3,17 @@
 import numpy as np
 import argparse
 from PIL import Image, ImageOps
-from typing import Tuple
+from time import sleep
 
 LONG_SPARSE_TO_DENSE = ' .\'`^",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 SHORT_SPARSE_TO_DENSE = ' .:-=+*#%@'
+
+
+def hide_cursor():
+    print('\033[?25l', end='')
+
+def show_cursor():
+    print('\033[?25h', end='')
 
 def color_str(r: int, g: int, b: int) -> str:
     return f'\033[48;2;{r};{g};{b}m'
@@ -47,12 +54,15 @@ def convert_to_ascii_grayscale(img: Image, ascii_scale: str) -> str:
         output += '\n'
     return output
 
+def resize(img: Image, scale: float) -> Image:
+    width, height = img.size
+    return img.resize((int(width / scale * 2), int(height / scale)))
+
 def convert_to_ascii(img: Image, args) -> str:
     ascii_scale = LONG_SPARSE_TO_DENSE
     if args.basic:
         ascii_scale = SHORT_SPARSE_TO_DENSE
-    width, height = img.size
-    img = img.resize((int(width // args.scale * 2), int(height // args.scale)))
+    img = resize(img, args.scale)
     if args.color:
         return convert_to_ascii_color(img)
     else:
@@ -62,7 +72,7 @@ def convert_to_ascii(img: Image, args) -> str:
         )
 
 def parse_args():
-    parser = argparse.ArgumentParser('img2ascii', description='Converts an image into ascii text')
+    parser = argparse.ArgumentParser('img2ascii', description='Converts an image or gif into ascii text')
     parser.add_argument(
         'filename',
         type=str,
@@ -99,17 +109,44 @@ def parse_args():
         action='store_true',
         help='use colors'
     )
+    parser.add_argument(
+        '-d',
+        '--delay',
+        type=float,
+        default=0.1,
+        help='Delay for displaying gifs in seconds'
+    )
     return parser.parse_args()
 
 def main():
     args = parse_args()
     img = Image.open(args.filename)
-    ascii_image = convert_to_ascii(img, args)
-    if args.output:
-        with open(args.output, 'w') as f:
-            f.write(ascii_image)
+    if not getattr(img, 'is_animated', False):
+        ascii_image = convert_to_ascii(img, args)
+        if args.output:
+            with open(args.output, 'w') as f:
+                f.write(ascii_image)
+        else:
+            print(ascii_image)
     else:
-        print(ascii_image)
+        frames = []
+        for i in range(img.n_frames):
+            img.seek(i)
+            frames.append(convert_to_ascii(img, args))
+        if args.output:
+            with open(args.output, 'w') as f:
+                for frame in frames:
+                    f.write(frame + '\n')
+        else:
+            hide_cursor()
+            try:
+                while True:
+                    for frame in frames:
+                        # escape code goes to positon 1 1 on screen
+                        print('\033[1;1H' + frame)
+                        sleep(args.delay)
+            except KeyboardInterrupt:
+                show_cursor()
 
 if __name__ == '__main__':
     main()
